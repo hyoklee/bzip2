15b5f01c91e8c8119e85635d7eed1cffd2394744
H. Joe Lee <hyoklee@hdfgroup.org>
2020-12-22 22:11:30 -0600

    OESS-104:Create bzip2 repo.

---
+++ config/cmake/cacheinit.cmake
@@ -0,0 +1,19 @@
+# This is the CMakeCache file.
+
+########################
+# EXTERNAL cache entries
+########################
+
+set (BUILD_SHARED_LIBS ON CACHE BOOL "Build Shared Libraries" FORCE)
+
+set (CMAKE_INSTALL_FRAMEWORK_PREFIX "Library/Frameworks" CACHE STRING "Frameworks installation directory" FORCE)
+
+set (BZ2_PACKAGE_EXT "" CACHE STRING "Name of bzip2 package extension" FORCE)
+
+set (BZ2_BUILD_UTILS ON CACHE BOOL "Build bzip2 Library Utilities" FORCE)
+
+set (BUILD_TESTING OFF CACHE BOOL "Build bzip2 Unit Testing" FORCE)
+
+set (BZ2_ENABLE_PARALLEL OFF CACHE BOOL "Enable parallel build (requires MPI)" FORCE)
+
+set (BZ2_PACKAGE_NAME "bz2" CACHE STRING "Name of bzip2 package" FORCE)
